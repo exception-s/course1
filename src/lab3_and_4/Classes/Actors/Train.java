0438606aa42ea24c189b8cd1f9878375efa6cfd2
exception <flqmeslayer@gmail.com>
2023-11-27 17:40:54 +0300

    UPD lab4

--- src/lab3_and_4/Classes/Actors/Train.java
+++ src/lab3_and_4/Classes/Actors/Train.java
@@ -11,5 +11,21 @@ public class Train extends TransportAbstract {
         System.out.println("Сейчас в " + place1.getPlaceName() + " на " + place2.getPlaceName() + " остановился " + getName());
     }
     class Engine{
+        private final int power;
+        private final String model;
+        private final int year;
+        public Engine(int pow, String model, int year){
+            this.power = pow;
+            this.model = model;
+            this.year = year;
+        }
+        public boolean isItWorking(){
+            return power >= 100;
+        }
+        public boolean isItRelevant(){
+            return year > 2020;
+        }
+        // etc
     }
+    Engine trainEngine = new Engine(100, "MegaCoolEngine", 2077);
 }
